988a18802bf4ca72c145e70dd74ca61ce8f00492
Emily <mei.gong@icloud.com>
2022-05-23 16:01:01 +1000

    fix: Clock.js/ use variable day from set time function in get current date function

--- src/clock/Clock.jsx
+++ src/clock/Clock.jsx
@@ -24,18 +24,28 @@ const Clock = (props) =>{
         setSecondDeg(second)
         setHour(day.getUTCHours())
 
+        // function getCurrentDate(separator='-'){
+        //     // let newDate = new Date();
+        //     // let day = new Date(nowTime.getTime() + props.timezone * 3600000);
+        //     let date = day.getDate();
+        //     let month = day.getMonth() + 1;
+        //     let year = day.getFullYear();
+        //     return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
+        // }
+        // setDay(getCurrentDate())
         const getCurrentDate =(separator='-')=>{
-            let newDate = new Date();
-            let day = new Date(nowTime.getTime() + props.timezone * 3600000);
+            // let newDate = new Date();
+            // let day = new Date(nowTime.getTime() + props.timezone * 3600000);
             let date = day.getDate();
-            let month = newDate.getMonth() + 1;
-            let year = newDate.getFullYear();
+            let month = day.getMonth() + 1;
+            let year = day.getFullYear();
             return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
         }
         setDay(getCurrentDate())
     }
 
     useEffect(()=>{
+        setTime()
         const interval = setInterval(()=>{
             setTime() //reset the time every 100
         }, 100)
